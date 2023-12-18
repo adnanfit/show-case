4929ede2366303e2ab6056c26ebcedbb9d77c3d4
Afaq Azhar <afaqazhar555@gmail.com>
2023-12-18 16:15:47 +0500

    Contact form emailJS

--- components/Contact/mailer.js
+++ components/Contact/mailer.js
@@ -1,11 +1,15 @@
 import emailjs from "@emailjs/browser";
 
+const serviceId = 'service_y74b5un';
+const templateId = 'template_xw94dte';
+const userKey = 'NNRph7K4nIFixWVeV';
+
 const mail = ({ name, email, message }) => {
   return emailjs.send(
-    process.env.NEXT_PUBLIC_SERVICE_ID,
-    process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    serviceId,
+    templateId,
     { name, email, message },
-    process.env.NEXT_PUBLIC_USER_ID
+    userKey
   );
 };
 
